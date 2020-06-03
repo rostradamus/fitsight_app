b9d2b5a300e5dc9073b6ee5f55364266942a96c5
Ro Lee <rolee0429@gmail.com>
2020-06-03 04:55:12 -0700

    Refactor listener

--- lib/widgets/pages/login_page.dart
+++ lib/widgets/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wolog_app/blocs/auth/auth_bloc.dart';
+import 'package:wolog_app/blocs/auth/auth_state.dart';
 import 'package:wolog_app/widgets/components/login/login_form.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,27 +9,49 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
-                fit: BoxFit.cover)),
-        child: Stack(
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.all(100.0),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                      child: Text("WOLOG",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic))),
-                )),
-            Padding(padding: EdgeInsets.all(30.0), child: LoginForm())
-          ],
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is LoginSuccessState) {
+            Navigator.of(context).pushNamed('/');
+          } else if (state is LoginFailureState) {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Try Again"),
+                    content: Text("Incorrect Email or Password."),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("Close"),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  );
+                });
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover)),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(100.0),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                        child: Text("WOLOG",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic))),
+                  )),
+              Padding(padding: EdgeInsets.all(30.0), child: LoginForm())
+            ],
+          ),
         ),
       ),
     );
